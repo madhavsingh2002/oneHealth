f195f60397295f7ceac5adcc03a914e2875a36f9
madhav <madhav201singh@gmail.com>
2024-10-18 23:55:45 +0530

    22

--- app/src/main/java/com/example/onehemlk/Pages/HomePage.kt
+++ app/src/main/java/com/example/onehemlk/Pages/HomePage.kt
@@ -1,14 +1,28 @@
 package com.example.onehemlk.Pages
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,19 +33,38 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.onehemlk.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomePage(navController: NavHostController) {
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    var selectedImageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+    val options = listOf("JPEG", "JPG", "PNG", "PDF", "WEBP")
+    val selectedOption = remember { mutableStateOf(options[0]) }
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            selectedImageUri = it
+            // Convert URI to Bitmap
+            selectedImageBitmap = MediaStore.Images.Media.getBitmap(
+                navController.context.contentResolver, it
+            )
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -40,18 +73,23 @@ fun HomePage(navController: NavHostController) {
                         modifier = Modifier.fillMaxWidth(),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text(text = "Home Page", fontSize = 16.sp, color = Color.White, textAlign = TextAlign.Center)
+                        Text(
+                            text = "Image Convertor",
+                            fontSize = 16.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
                     }
-                },  // App bar title
+                },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary // Background color
+                    containerColor = MaterialTheme.colorScheme.primary
                 )
             )
         },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    // Action when the FAB is clicked
+                    imagePickerLauncher.launch("image/*")  // Open the image picker
                 },
                 modifier = Modifier.width(150.dp),
                 containerColor = MaterialTheme.colorScheme.tertiary
@@ -59,26 +97,110 @@ fun HomePage(navController: NavHostController) {
                 Row {
                     Icon(
                         imageVector = Icons.Default.Add,
-                        contentDescription = "Add Icon",  // Add a content description for accessibility
+                        contentDescription = "Add Icon",
                         tint = Color.White
                     )
-                    Spacer(modifier = Modifier.width(8.dp))  // Space between icon and text
+                    Spacer(modifier = Modifier.width(8.dp))
                     Text(text = "Select Image", color = Color.White)
                 }
             }
         }
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
+
                 .background(MaterialTheme.colorScheme.primary),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Home Page Content",  // Page content
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.secondary
-            )
+            // Display the selected image, if any
+            if (selectedImageBitmap != null) {
+                Image(
+                    bitmap = selectedImageBitmap!!.asImageBitmap(),
+                    contentDescription = "Selected Image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.bannerimage_1),
+                    contentDescription = "Banner Image"
+                )
+            }
+            Spacer(modifier = Modifier.height(18.dp))
+            if (selectedImageBitmap != null) {
+                Text(
+                    text = "Select images",
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+            else{
+                Text(
+                    text = "Select File Format",
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                // Segmented buttons implementation
+                SegmentedButton(
+                    options = options,
+                    selectedOption = selectedOption.value,
+                    onOptionSelected = { selectedOption.value = it }
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                // Display the selected option
+                Text(
+                    text = "Selected: ${selectedOption.value}",
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.secondary,
+                    textAlign = TextAlign.Center
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun SegmentedButton(
+    options: List<String>,
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        options.forEach { option ->
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .border(
+                        width = 1.dp ,
+                        color = MaterialTheme.colorScheme.tertiary,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .clickable { onOptionSelected(option) }
+                    .background(
+                        color = if (option == selectedOption) MaterialTheme.colorScheme.tertiary else Color.Transparent
+                    )
+                    .padding(8.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = option,
+                    color = if (option == selectedOption) Color.White else MaterialTheme.colorScheme.secondary,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
